59e8f9f099a5ab3cdc5a41cec3e595c2fc4d6178
fabricio <fabriciojf@gmail.com>
2017-09-28 08:44:10 -0300

    adicao do projeto no github

---
+++ file_to_array.php
@@ -0,0 +1,16 @@
+<?php
+
+$ddl = file('./file_to_array.in');
+
+$content = '';
+
+foreach($ddl as $line)
+{
+    if (strpos(strtoupper($line), 'CREATE TABLE') !== false) {
+        $content .= $line;
+    }
+}
+
+file_put_contents('file_to_array.out', $content);
+
+?>
